00a00f9039ae2fac0c9495ad7adda1fc295dd62c
Takeshi Ueda <uedatakeshi@gmail.com>
2020-09-05 20:46:18 +0900

    modify check routine

--- read.php
+++ read.php
@@ -16,13 +16,14 @@ echo $kabus->apikey . "\n";
 //$order_id = $kabus->dummyOrder("1234", 100);
 //$order_id = $kabus->getsendorder("8550", 213);
 
-exit;
+//exit;
 
 // 最初にAPI銘柄リストをリセット
 $kabus->removeAll();
 
 $list = $codes->allCode;// 本日の対象銘柄のリスト
 $db = pg_connect("host=localhost dbname=" . DB_NAME. " user=" . DB_USER . " password=" . DB_PASS);
+
 $loop = 1;
 $ordered = 0;
 while (1) {
@@ -73,10 +74,14 @@ function checkOrder($symbol, $loop) {
     list($c4, $c3, $c2, $c1) = $loop_array; 
     $loop_in = implode(",", $loop_array);
     $reg_date = date("Y-m-d");
+    //$reg_date = "2020-09-04";
     $output = [];
     $query = <<<END
     SELECT 
-        loop, CurrentPrice, CurrentPriceTime, CurrentPriceStatus, BidPrice, vwap, ChangePreviousClosePer, tradingvolume
+        Symbol, 
+        loop, CurrentPrice, CurrentPriceTime, CurrentPriceStatus, 
+        BidPrice, vwap, ChangePreviousClosePer, tradingvolume, 
+        openingprice, lowprice
     FROM items
     WHERE symbol='{$symbol}' AND loop IN ({$loop_in}) and reg_date='{$reg_date}'
     ORDER BY loop DESC 
@@ -94,47 +99,13 @@ END;
                 $output[$myloop]['vwap'] = $row['vwap'];
                 $output[$myloop]['changepreviouscloseper'] = $row['changepreviouscloseper'];
                 $output[$myloop]['tradingvolume'] = $row['tradingvolume'];
+                $output[$myloop]['openingprice'] = $row['openingprice'];
+                $output[$myloop]['lowprice'] = $row['lowprice'];
             }
         }
     }
-    if (isset($output[$c4]['currentpricestatus'])) {
-    	$currentpricestatus = $output[$c4]['currentpricestatus'];
-    } else {
-    	return false;
-    }
-    if (isset($output[$c4]['bidprice'])) {
-    	$bidprice = $output[$c4]['bidprice'];
-    } else {
-    	return false;
-    }
-    if (isset($output[$c4]['price']) && isset($output[$c3]['price'])) {
-        $diff1 = $output[$c4]['price'] - $output[$c3]['price'];
-        $vdiff1 = $output[$c4]['tradingvolume'] - $output[$c3]['tradingvolume'];
-    }
-    if (isset($output[$c3]['price']) && isset($output[$c2]['price'])) {
-        $diff2 = $output[$c3]['price'] - $output[$c2]['price'];
-        $vdiff2 = $output[$c3]['tradingvolume'] - $output[$c2]['tradingvolume'];
-    }
-    if (isset($output[$c2]['price']) && isset($output[$c1]['price'])) {
-        $diff3 = $output[$c2]['price'] - $output[$c1]['price'];
-        $vdiff3 = $output[$c2]['tradingvolume'] - $output[$c1]['tradingvolume'];
-    }
-    $prate = 0;
-    if (isset($output[$c4]['price']) && ($output[$c4]['price'] > 0)) {
-        $prate = round(100 * $diff1 / $output[$c4]['price'], 2);// 現在価格の上昇率
-    }
-    $vrate = 0;
-    if (isset($output[$c4]['tradingvolume']) && ($output[$c4]['tradingvolume'] > 0)) {
-        $vrate = round(100 * $vdiff1 / $output[$c4]['tradingvolume'], 2);// 現在出来高の上昇率
-    }
-    $wrate = 0;
-    if (isset($output[$c4]['vwap']) && ($output[$c4]['vwap'] > 0)) {
-        $wrate = round(100 * $output[$c4]['price'] / $output[$c4]['vwap'], 2);// VWAPの乖離率
-    }
-    if (($wrate < 101.6) && ($vrate > 15) && ($vrate < 50) && ($prate > 0.5) && ($diff1 > $diff2) && ($diff2 >= $diff3) && ($diff3 > 0) && ($vdiff1 > $vdiff2) && ($vdiff2 > $vdiff3) && ($vdiff3 > 0) && ($currentpricestatus == 1)) {
-        return $bidprice;
-    }
-    return false;
+
+    return calcFourth($output, $loop_array);
 }
 
 function insQuery($item, $loop) {
@@ -226,3 +197,90 @@ END;
 
     return $query;
 }
+
+function calcFourth($output, $loop_array) {
+    list($c4, $c3, $c2, $c1) = $loop_array; 
+    $j_time = mktime(10, 20, 0);
+
+    if (isset($output[$c4]['currentpricestatus'])) {
+    	$currentpricestatus = $output[$c4]['currentpricestatus'];
+    } else {
+    	return false;
+    }
+    if (isset($output[$c4]['bidprice'])) {
+    	$bidprice = $output[$c4]['bidprice'];
+    } else {
+    	return false;
+    }
+    if (isset($output[$c4]['price']) && isset($output[$c3]['price'])) {
+        $diff1 = $output[$c4]['price'] - $output[$c3]['price'];
+        $vdiff1 = $output[$c4]['tradingvolume'] - $output[$c3]['tradingvolume'];
+    } else {
+        return false;
+    }
+    if (isset($output[$c3]['price']) && isset($output[$c2]['price'])) {
+        $diff2 = $output[$c3]['price'] - $output[$c2]['price'];
+        $vdiff2 = $output[$c3]['tradingvolume'] - $output[$c2]['tradingvolume'];
+    } else {
+        return false;
+    }
+    if (isset($output[$c2]['price']) && isset($output[$c1]['price'])) {
+        $diff3 = $output[$c2]['price'] - $output[$c1]['price'];
+        $vdiff3 = $output[$c2]['tradingvolume'] - $output[$c1]['tradingvolume'];
+    } else {
+        return false;
+    }
+    $prate = 0;
+    if (isset($output[$c4]['price']) && ($output[$c4]['price'] > 0)) {
+        $prate = round(100 * $diff1 / $output[$c4]['price'], 2);// 現在価格の上昇率
+    } else {
+        return false;
+    }
+    $vrate = 0;
+    if (isset($output[$c4]['tradingvolume']) && ($output[$c4]['tradingvolume'] > 0)) {
+        $vrate = round(100 * $vdiff1 / $output[$c4]['tradingvolume'], 2);// 現在出来高の上昇率
+    } else {
+        return false;
+    }
+    $wrate = 0;
+    if (isset($output[$c4]['vwap']) && ($output[$c4]['vwap'] > 0)) {
+        $wrate = round(100 * $output[$c4]['price'] / $output[$c4]['vwap'], 2);// VWAPの乖離率
+    } else {
+        return false;
+    }
+    if (isset($output[$c4]['openingprice'])) {
+        $drate = 100 * ($output[$c4]['price'] - $output[$c4]['openingprice']) / $output[$c4]['openingprice'];
+    } else {
+        return false;
+    }
+    if (isset($output[$c4]['time']) && preg_match("/^(\d+)-(\d+)-(\d+) (\d+):(\d+):(\d+)/", $output[$c4]['time'], $regs)) {
+        $h = $regs[4];
+        $m = $regs[5];
+        $b = $regs[6];
+        $c_time = mktime($h, $m, $b);
+        if ($c_time > $j_time) {
+            return false;
+        }
+    } else {
+        return false;
+    }
+    if (isset($output[$c4]['openingprice'])) {
+        if ($output[$c4]['price'] < $output[$c4]['openingprice']) {
+            return false;
+       }
+    } else {
+        return false;
+    }
+
+    if (($diff1 > $diff2) && ($diff2 >= $diff3) && ($diff3 > 0)) {
+        if (($vdiff1 > $vdiff2) && ($vdiff2 > $vdiff3) && ($vdiff3 > 0)) {
+            if (($wrate < 101.6)  && ($prate > 0.5)) {
+                return $bidprice;
+            }
+        }
+    }
+
+    return false;
+
+}
+
